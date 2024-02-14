1c0c11a573ab84be202a3c348c953d815bf86539
Anna Zhuravleva <ansiv@yandex.ru>
2024-02-14 16:41:11 +0300

    feat: add error handle & class tests

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,19 +1,19 @@
-// Uncomment the code below and write your tests
-import { throwError, resolveValue } from './index';
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import {
+  throwError,
+  throwCustomError,
+  resolveValue,
+  MyAwesomeError,
+  rejectCustomError,
+} from './index';
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
-    const data = await resolveValue(125);
-    expect(data).toBe(125);
-  });
+    const num = await resolveValue(125);
+    expect(num).toBe(125);
 
-  test('should resolve provided value', async () => {
-    const data = await resolveValue('someStr');
-    expect(data).toBe('someStr');
-  });
+    const str = await resolveValue('someStr');
+    expect(str).toBe('someStr');
 
-  test('should resolve provided value', async () => {
     const data = await resolveValue(null);
     expect(data).toBeNull();
   });
@@ -21,10 +21,29 @@ describe('resolveValue', () => {
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
-    expect(throwError('msg')).toThrow('msg');
+    expect(() => {
+      throwError('msg');
+    }).toThrow('msg');
   });
 
   test('should throw error with message if message is not provided', () => {
-    expect(throwError()).toThrow('Oops!');
+    expect(() => {
+      throwError();
+    }).toThrow('Oops!');
+  });
+});
+
+describe('throwCustomError', () => {
+  test('should throw custom error', () => {
+    expect(() => {
+      throwCustomError();
+    }).toThrow(MyAwesomeError);
+  });
+});
+
+describe('rejectCustomError', () => {
+  test('should reject custom error', async () => {
+    expect.assertions(1);
+    await expect(rejectCustomError()).rejects.toEqual(new MyAwesomeError());
   });
 });
